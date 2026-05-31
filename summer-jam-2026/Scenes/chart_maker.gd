17ed6874953af68df6b7fcb887aff2088f72859e
Casey Bracken <bracc004@gmail.com>
2026-05-31 13:49:07 +0100

    song chart

--- summer-jam-2026/Scenes/chart_maker.gd
+++ summer-jam-2026/Scenes/chart_maker.gd
@@ -8,16 +8,28 @@ extends Node2D
 @export var notePS: PackedScene
 @export var followCam: Camera2D
 @export var chartParent: Node2D
+@export var timeLabel: Label
 
 @onready var recording: bool = false
+@onready var paused: bool = false
 
 func _ready() -> void:
 	player.connect("finished", stop_charting)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	timeLabel.text = "Paused: {0}\nTime: {1}".format([paused, player.get_playback_position()])
+	
 	if Input.is_action_just_pressed("ui_accept"):
-		start_charting()
+		if !recording:
+			start_charting()
+		else:
+			if !paused:
+				player.stream_paused = true
+				paused = true
+			else:
+				player.stream_paused = false
+				paused = false
 	
 	if recording:
 		var dist = -(chartSpeed * player.get_playback_position())
