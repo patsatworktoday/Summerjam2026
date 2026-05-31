0ed2dc41a1fddec026f6a2a5fab6c20e13105d6f
Casey Bracken <bracc004@gmail.com>
2026-05-31 14:47:46 +0100

    updated rhythm game script & background

--- summer-jam-2026/Scenes/note_lane.gd
+++ summer-jam-2026/Scenes/note_lane.gd
@@ -10,19 +10,27 @@ extends Node2D
 @export var chart: Chart
 @export var chartSpeed: float
 
+@export_category("Music")
+@export var musicPlayer: AudioStreamPlayer
+
 @onready var hits: float = 0
 @onready var misses: float = 0
 
+var playDelay: Timer
 var laneNotePresent: Dictionary[Area2D, Area2D]
 
-signal reset_chart
+signal end_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for lane in lanes:
 		laneNotePresent.set(lane, blank)
-	connect("reset_chart", chart.reset_chart)
-	chart.transform.origin.y = -300
+	chart.transform.origin.y = -320
+	playDelay = Timer.new()
+	add_child(playDelay)
+	playDelay.one_shot = true
+	playDelay.timeout.connect(musicPlayer.play)
+	playDelay.start(5.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -32,40 +40,31 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed(action):
 			laneAudioMap.get(action).play()
 			if note != blank:
-				print("note hit on {0}".format([lane.name]))
 				chart.disable_note(note)
 				hits += 1
 				print("hits: {0}".format([hits]))
 			else:
-				print("no note present, missed!")
 				misses += 1
 				print("misses: {0}".format([misses]))
 	
 	chart.transform.origin.y += delta * chartSpeed
-	
-	# quick reset
-	if Input.is_action_just_pressed("ui_accept"):
-		print("resetting chart!\nhits: {0}\nmisses: {1}\naccuracy: {2}".format([hits, misses, hits/(hits+misses)]))
-		hits = 0
-		misses = 0
-		emit_signal("reset_chart")
-		chart.transform.origin.y = -300
 
 # Note enters the input area, so a note is present
 func _on_note_area_entered(note: Area2D, lane: Area2D) -> void:
-	print("note entered on {0}".format([lane.name]))
 	laneNotePresent[lane] = note
 
 # Note exits input area, so a note isn't present
 func _on_note_area_exited(note: Area2D, lane: Area2D) -> void:
-	print("note exited on {0}".format([lane.name]))
 	# We want the player to target the "freshest" note
 	if laneNotePresent[lane] == note:
 		laneNotePresent[lane] = blank
 
 # Note goes past the input zone & is missed by the player
 func _on_miss_zone_entered(note: Area2D) -> void:
-	print("note missed")
 	chart.disable_note(note)
 	misses += 1
 	print("misses: {0}".format([misses]))
+
+func end_chart():
+	print("ending game")
+	end_game.emit()
